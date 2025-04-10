eaa645c251a27220237c2d06213bd286311f1aba
Shohrux <shohruxnizomov170@gmail.com>
2025-04-10 20:32:14 +0500

    websocket kutubxonalari import qilindi

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO  # SocketIO ni boshida import qilamiz
 
 app = Flask(__name__)
-CORS(app)
-# PostgreSQL URL config: (username, password, host, port, dbname)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/twitter_clone'
+CORS(app)  # HTTP so‘rovlar uchun CORS
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
+db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Faqat bitta socketio obyekti
+
+# Route’lar va modellar import qilinadi
 from routes import *
+from models import *
+
+
 with app.app_context():
     db.create_all()
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    socketio.run(app, host='0.0.0.0', port=5051, debug=True)
