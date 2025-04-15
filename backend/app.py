be9cbdbc8dc64732c6407791c733bcbf8475a99c
Shohrux <shohruxnizomov170@gmail.com>
2025-04-15 20:22:21 +0500

    update routes.py
    # Conflicts:
    #	backend/routes.py

--- backend/app.py
+++ backend/app.py
@@ -19,4 +19,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5051, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5050, debug=True)
